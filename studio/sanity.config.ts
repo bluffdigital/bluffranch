815bf6a9460649f0a608c5c91d9025ee2e498a7e
bluffdigital <doug@bluffdigital.com>
2025-05-03 17:34:56 -0500

    Fixing studio integration.

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -48,6 +48,7 @@ function resolveHref(documentType?: string, slug?: string): string | undefined {
 
 // Main Sanity configuration
 export default defineConfig({
+  basePath: '/studio',
   name: 'default',
   title: 'Bluff Ranch',
 
