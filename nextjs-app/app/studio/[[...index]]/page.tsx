3edadfc882211e70d7d036a71445c6e6e6dc40c0
bluffdigital <doug@bluffdigital.com>
2025-05-04 21:23:31 -0500

    Moved a bunch of files, broke and fixed the site, but I believe this fits the 'monorepo' definition.

--- nextjs-app/app/studio/[[...index]]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { NextStudio } from 'next-sanity/studio';
-import config from '../../../studio/sanity.config'
-
-export default function StudioPage() {
-    // @ts-ignore
-    return <NextStudio config={config} />;
-}
